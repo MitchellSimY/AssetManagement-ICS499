08f0cc67536bdb57e55a79443b696d57c1231832
Mitch Y <mitchellsimy@gmail.com>
2022-02-02 15:51:35 -0600

    Updated packages
    Added React Router

---
+++ assetmanagement_frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default function Student() {
+
+    return (
+        <div>
+            <h2>
+                Hello you are home
+            </h2>
+        </div>
+    );
+}
