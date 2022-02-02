b45eb4daa26be7f7f7aee2540e2738963623ea3b
Mitch Y <mitchellsimy@gmail.com>
2022-02-02 12:00:47 -0600

    Initial Commit
    Initial Commit

---
+++ AssetManagement/src/test/java/Group1/AssetManagement/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Group1.AssetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
